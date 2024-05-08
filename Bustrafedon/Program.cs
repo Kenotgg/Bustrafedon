827439afaf9d295eac4c2c2bff57dc564c2165a3
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2024-05-08 10:14:08 +0500

    ToTXT

--- Bustrafedon/Program.cs
+++ Bustrafedon/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace Bustrafedon
 {
 	internal class Program
@@ -12,43 +10,180 @@ namespace Bustrafedon
 		static void Main(string[] args)
 		{
 			//GitHub
+
 			///Нужно такое понятие как строка, пока попробую массивом строк бустрафайдить одну строку из массива на вход
 			//Получение текста от пользователя
-			string[] textStrings = new string[4];
-			textStrings[0] = "Октябрь уж наступил — уж роща отряхает";
-			textStrings[1] = "Последние листы с нагих своих ветвей;";
-			textStrings[2] = "Дохнул осенний хлад — дорога промерзает.";
-			textStrings[3] = "Журча еще бежит за мельницу ручей.";
-			foreach (string s in textStrings)
+			//string[] textStrings = new string[500000];
+			List<string> textStrings = new List<string>();
+			//textStrings[0] = "Октябрь уж наступил — уж роща отряхает";
+			//textStrings[1] = "Последние листы с нагих своих ветвей;";
+			//textStrings[2] = "Дохнул осенний хлад — дорога промерзает.";
+			//textStrings[3] = "Журча еще бежит за мельницу ручей.";
+
+			//консольный ввод
+			//Console.WriteLine("Чтобы закончить ввод нажмите Esc:");
+			//bool userInput = true;
+			//string userText;
+			//int i = 0;
+			//while (userInput)
+			//{
+			//	Console.WriteLine("Введите строку:");
+			//	userText = Console.ReadLine();
+			//	if (userText == "")
+			//	{
+			//		break;
+			//	}
+			//	textStrings[i] = userText;
+			//	i++;
+
+			//}
+
+			//вариант с копипастом из файла
+			string path = "UserText.txt";
+			//передаем построчно то что находится в файле в массив textStrings
+			using(StreamReader sr = new StreamReader(path, Encoding.UTF8)) 
 			{
-				Console.WriteLine(s);
+				string line;
+				//int lineCounter = 0;
+				while ((line = sr.ReadLine()) != null)
+				{
+					//lineCounter++;
+					Console.WriteLine(line);
+					textStrings.Add(line);
+				}
 			}
+
+			//foreach (string s in textStrings)//вывели исходный текст
+			//{
+			//	if(s != null)
+			//	Console.WriteLine(s);
+			//}
 			Console.WriteLine();
-			//Разворото каждой второй строки
-			BustrafideText(textStrings);
+
+
+			//BustrafideText(textStrings);//Должно вывести текст, где каждая 2 строка развернута в обратную сторону
+
+			BustrafideAndWriteToTXT(textStrings);
+
+			//Console.WriteLine();
+			//string outputPath = "ProcessedText.txt";
+			//using (StreamWriter sw = new StreamWriter(outputPath))
+			//{
+			//	foreach (string line in textStrings)
+			//	{
+			//		sw.WriteLine(line);
+			//	}
+			//}
+
+			//написать текст в документ
+			//for (int i = 0; i < textStrings.Count; i++)
+			//{
+			//	using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+			//	{
+			//		sw.WriteLine(textStrings[i]);
+			//	}
+			//}
+
 			Console.ReadKey();
 		}
-		static void BustrafideText(string[] text)
+		static void BustrafideText(List<string> text)
 		{
 			//Получаем текст
+			//List<string> bustrafidedText = new List<string>();
 			
-
-				for (int i = 1; i <= text.Length; i += 2)
-				{
-					
-					text[i].ToCharArray();
-					for (int j = text[i].Length - 1; j >= 0; j--)
-					{
-						Console.Write(text[i][j]);
-					}
-				}
-				Console.WriteLine();
-			
+			//Печатает первую, после 2 в обратной и т д
 			//Берем каждую второю строку
 			
+			for (int i = 0; i < text.Count; i++)
+			{
+				//можно в одну text[i] != null вынести
+				if (i % 2 == 1)
+				{
+					if (text[i] != null && text[i] != " ")
+					{
+						text[i].ToCharArray();
+						for (int j = text[i].Length - 1; j >= 0; j--)
+						{
+							Console.Write(text[i][j]);							
+						}
+					}
+				}
+				else
+				{
+					if (text[i] != null && text[i] != " ")
+					{
+						Console.Write(text[i]);
+						
+						//sw.Write(text[i]);
+					}
+
+				}
+				if (text[i] != null && text[i] != " ")
+				{
+					Console.WriteLine();
+					
+				}
+
+			}
+
+			//записать в файл текст
 			
+			
+
 			//Разворачиваем ее
 			//Выводим весь текст
+		}
+		static void BustrafideAndWriteToTXT(List<string> text)
+		{
+			//Получаем текст
+			//List<string> bustrafidedText = new List<string>();
+
+			//Печатает первую, после 2 в обратной и т д
+			//Берем каждую второю строку
+			string outputPath = "ProcessedText.txt";
+			//using (StreamWriter sw = new StreamWriter(outputPath))
+			using (StreamWriter streamWriter = new StreamWriter(outputPath))
+			{
+				for (int i = 0; i < text.Count; i++)
+				{
+					//можно в одну text[i] != null вынести
+					if (i % 2 == 1)
+					{
+						if (text[i] != null && text[i] != " ")
+						{
+							text[i].ToCharArray();
+							for (int j = text[i].Length - 1; j >= 0; j--)
+							{
+								//Console.Write(text[i][j]);
+								streamWriter.Write(text[i][j]);
+							}
+						}
+					}
+					else
+					{
+						if (text[i] != null && text[i] != " ")
+						{
+							//Console.Write(text[i]);
+
+							streamWriter.Write(text[i]);
+						}
+
+					}
+					if (text[i] != null && text[i] != " ")
+					{
+						//Console.WriteLine();
+						streamWriter.WriteLine();
+					}
+
+				}
+
+			}
+
+			//записать в файл текст
+
+
+
+			//Разворачиваем ее
 		}
 
 	}
